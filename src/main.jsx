8aef53afac682e5ef41774fc3f8cb86d1f666a8c
Leo W <leo.westebbe@gmail.com>
2023-01-25 10:52:34 -0500

    Switch to createRoutesFromElements

--- src/main.jsx
+++ src/main.jsx
@@ -5,35 +5,49 @@ import "./reset.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Homepage from "./features/homepage/Homepage";
 import Subreddit from "./features/subreddit/Subreddit";
 import SearchResults from "./features/search/SearchResults";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Homepage />,
-      },
-      {
-        path: ":sortType",
-        element: <Homepage />,
-      },
-      {
-        path: "r/:subredditName",
-        element: <Subreddit />,
-      },
-      {
-        path: "search?",
-        element: <SearchResults />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/:sortType?" element={<App />}>
+      <Route index element={<Homepage />} />
+      <Route path="r/:subredditName/:sortType?" element={<Subreddit />} />
+      <Route path="search?/:sortType?" element={<SearchResults />} />
+    </Route>
+  )
+  //   [
+  //   {
+  //     path: "/",
+  //     element: <App />,
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <Homepage />,
+  //       },
+  //       {
+  //         path: ":sortType",
+  //         element: <Homepage />,
+  //       },
+  //       {
+  //         path: "r/:subredditName",
+  //         element: <Subreddit />,
+  //       },
+  //       {
+  //         path: "search?",
+  //         element: <SearchResults />,
+  //       },
+  //     ],
+  //   },
+  // ]
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
