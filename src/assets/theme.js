e83153c8157f3c6a39133354a1526eb04d8cecc1
Leo W <leo.westebbe@gmail.com>
2023-02-17 14:47:38 -0500

    fix width and padding for root container

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,8 +1,16 @@
 import { createTheme } from "@mui/system";
 
 const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "2rem",
+    },
+  },
   components: {
     // Name of the component
+    MuiContainer: {
+      styleOverrides: { root: { maxWidth: "100vw", padding: "0" } },
+    },
     MuiButtonBase: {
       defaultProps: {
         // The props to change the default for.
