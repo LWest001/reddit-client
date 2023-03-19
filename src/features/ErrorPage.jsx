c43c00b2b855ce6cc8bd003c65224a6bb33b8a7b
Leo W <leo.westebbe@gmail.com>
2023-03-19 11:29:55 -0400

    MUI ErrorPage complete

--- src/features/ErrorPage.jsx
+++ src/features/ErrorPage.jsx
@@ -1,19 +1,41 @@
 import { Link, useRouteError } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setStatus } from "./ThreadList/threadListSlice";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Stack,
+  Typography,
+} from "@mui/material";
+import errorLogo from "/errorLogo.svg";
+import theme from "../assets/theme";
 
 const ErrorPage = () => {
   let error = useRouteError();
   console.log(error);
   const dispatch = useDispatch();
   return (
-    <div className="ErrorPage">
-      <h2 className="errorText">Oops! We have encountered an error.</h2>
-      <br />
-      <Link to="/" onClick={() => dispatch(setStatus("idle"))}>
-        <button>Return home</button>
-      </Link>
-    </div>
+    <Card
+      className="ErrorPage"
+      sx={{ background: theme.palette.headerGradient.default }}
+    >
+      <CardContent>
+        <Stack direction="row" sx={{ alignItems: "center", gap: 1, m: 2 }}>
+          <img src={errorLogo} width="100px" />
+          <Typography className="errorText" fontSize="2rem" textAlign="start">
+            Oops! We have encountered an error.
+          </Typography>
+        </Stack>
+      </CardContent>
+      <Button
+      variant="contained"
+        component={Link}
+        to="/"
+        onClick={() => dispatch(setStatus("idle"))}
+      >Return home</Button>
+    </Card>
   );
 };
 export default ErrorPage;
