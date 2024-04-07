37fa42f832755490c3b2cbdd2d98f3328f834f56
Leo W <leo.westebbe@gmail.com>
2024-04-06 20:11:19 -0400

    push to netlify

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 function App() {
+  //
   return (
     <QueryClientProvider client={queryClient}>
       <CssBaseline />
